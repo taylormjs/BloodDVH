72bd7968d4fb35a77af91b0b25b77e0c963ca4c8
Zhaoqi Wang <wangzhaoqi930829@hotmail.com>
2017-05-30 16:20:56 -0400

    ...

--- 1Dbloodflow.py
+++ 1Dbloodflow.py
@@ -6,6 +6,7 @@ Created on Tue May 30 10:50:21 2017
 @author: taylorsorenson, zhaoqiwang
 """
 
+import numpy as np
 #Attempting to push to github
 
 
@@ -13,13 +14,14 @@ class Blood(object):
     ''' '''
     def __init__(self):
         self.dose_added = 0
+        self.getting_dose = False;
         
     def set_dose(self,dose):
         self.dose_added += dose
         
     def get_dose(self):
         return self.dose_added
-
+    
 
 class Position(object):
     def __init__(self, dose):
@@ -39,17 +41,31 @@ def make_blood(num_blood_cells):
         
 def find_blood():
     '''finds which blood cells are in the path of the beam
-    '''       
+    ''' 
+    if blood.position is in        
         
-def add_constant_dose(blood, dose):
+def add_constant_dose(all_bloods, dose):
     '''
     add a constant dose of radiation to all blood within the radiation beam
     blood - a list of blood objects that are
     '''
-    #time?
+    for i in all_bloods:
+        if i.getting_dose == True;
+            i.setdose(dose)
     
-def simulate_blood_flow(total_time, dt):
-    pass
+def bloodflow(blood,position,dt):
+    velocity = position(blood.position)
+    x = blood.position[0]
+    y = blood.position[1]
+    z = blood.position[3]
+
+
+def simulate_blood_flow(all_bloods,Positions,total_time, dt):
+    t = 0;
+    while t <= total_time:
+        add_constant_dose(all_bloods,dose) 
+        blood_flow(all_bloods,)
+        t = t + dt
         
 def make_pdf(blood):
     pass

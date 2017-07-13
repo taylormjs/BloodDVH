1bbe745bdb8ae3ed69e6c637ed349f0c7f5879c4
Zhaoqi Wang <wangzhaoqi930829@hotmail.com>
2017-07-13 17:17:46 -0400

    add main and dose class

--- __main__.py
+++ __main__.py
@@ -1,7 +1,25 @@
-from Blood import Blood
 from VectorFields import VectorFields
 from BloodPlot import *
 from Position import Position
+from Dose import Dose
+from BloodSimulation import  BloodSimulation
+import scipy.io
+import numpy as np
+
+
 
 def main():
+
     simulation = BloodSimulation()
+    mat_v = scipy.io.loadmat('VELOCITYVECSbloodflow.mat')  # read velocity field from mat file
+    vx = np.array(mat_v['u'])
+    vy = np.array(mat_v['v'])
+    vz = np.array(mat_v['w'])
+    mat_d = scipy.io.loadmat('DOSESbloodflow.mat')  # read dose field
+    velocity_field = VectorFields(vx,vy,vz)
+    mat_d = scipy.io.loadmat('DOSESbloodflow.mat')  # read dose field
+    dr = np.array(mat_d['dr'])
+    doses_object = Dose([dr],[1],[1])
+    blood_density = 1
+    dt = 0.1
+    simulator = BloodSimulation( velocity_field, doses_object, blood_density, dt)

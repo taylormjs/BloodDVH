51d627efb22fe469dcfadafe4739022b2864e258
Taylor Sorenson <taylorsorenson@Taylors-MacBook-Pro-3.local>
2017-07-26 15:56:15 -0400

    ploting relative cvhs

--- BloodPlot.py
+++ BloodPlot.py
@@ -60,8 +60,8 @@ def graphAndSaveDVHPlots(bin_list, dvh_list, num_bloods_per_set, styles_list, le
     for i in range(len(num_bloods_per_set)):
         bin_centers, dvh = bin_list[i], dvh_list[i]
         num_bloods = num_bloods_per_set[i]
-        ax.plot(bin_centers,  (num_bloods - dvh), styles_list[i], label=legend_list[i])
-        # ax.plot(bin_centers, (num_bloods - dvh) / num_bloods * 100, styles_list[i],label=legend_list[i])
+        # ax.plot(bin_centers,  (num_bloods - dvh), styles_list[i], label=legend_list[i])
+        ax.plot(bin_centers, (num_bloods - dvh) / num_bloods * 100, styles_list[i],label=legend_list[i])
         ax.legend()
     # num_bloods = len(data_sets[0]) #TODO - double check this
     plt.title("Blood Dose-Volume Histogram\n Total # of Blood Voxels: " + str(num_bloods_per_set))
